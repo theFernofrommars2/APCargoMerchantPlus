9faea2ea4126a106d175f4dd38253560a4c52fec
cccm5 <danieldavidson@aol.com>
2016-11-04 18:41:15 -0400

    First push

---
+++ src/main/io/github/cccm5/Main.java
@@ -0,0 +1,129 @@
+package io.github.cccm5;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.CraftManager;
+import net.countercraft.movecraft.utils.MovecraftLocation;
+public class Main extends JavaPlugin implements Listener {
+
+    private CraftManager craftManager;
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+        craftManager = CraftManager.getInstance();
+    }
+
+    public void onDisable() {
+
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) { // Plugin
+        Player player = (Player) sender;
+        if (command.getName().equalsIgnoreCase("unload") && sender instanceof Player) {
+            return true;
+        }
+        if (command.getName().equalsIgnoreCase("load") && sender instanceof Player) {
+            return true;
+        }
+        return false;
+
+    }
+
+    @EventHandler
+    public void onSignClick(PlayerInteractEvent e) {
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (e.getClickedBlock().getType() == Material.SIGN || e.getClickedBlock().getType() == Material.SIGN_POST) {
+                Sign sign = (Sign) e.getClickedBlock().getState();
+                Player p = e.getPlayer();
+                if (sign.getLine(0).equals(ChatColor.GREEN + "[Load]")) {
+                    Craft craft = craftManager.getCraftByPlayer(e.getPlayer());
+                    ItemStack heldItem = p.getInventory().getItemInMainHand();
+                    if(craft!=null && heldItem !=null){
+                        for(Inventory inv : Utils.getInventorysOnCraft(craft,p.getInventory().getItemInMainHand())){
+                            for(ItemStack playerStack : p.getInventory().getContents()){
+                                if(playerStack.isSimilar(heldItem)){
+                                    int limit = Utils.addLimit(inv,playerStack);
+                                    if(limit>0){
+                                        if(Utils.hasSpace(inv,playerStack)){
+                                            inv.addItem(playerStack);
+                                            p.getInventory().removeItem(playerStack);
+                                        }else{
+                                            ItemStack itemClone = playerStack.clone();
+                                            itemClone.setAmount(limit);
+                                            inv.addItem(itemClone);
+                                            p.getInventory().removeItem(itemClone);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    return;
+                }
+
+                if (sign.getLine(0).equals(ChatColor.GREEN + "[UnLoad]")) {
+
+                    Craft craft = craftManager.getCraftByPlayer(e.getPlayer());
+                    ItemStack heldItem = p.getInventory().getItemInMainHand();
+                    Inventory playerInv = p.getInventory();
+                    if(craft!=null && heldItem !=null){
+                        for(Inventory inv : Utils.getInventorysOnCraft(craft,p.getInventory().getItemInMainHand())){
+                            for(ItemStack cargoStack : inv){
+                                if(cargoStack.isSimilar(heldItem)){
+                                    int limit = Utils.addLimit(playerInv,cargoStack);
+
+                                    if(limit>0){
+                                        if(Utils.hasSpace(playerInv,cargoStack)){
+                                            inv.addItem(cargoStack);
+                                            p.getInventory().removeItem(cargoStack);
+                                        }else{
+                                            ItemStack itemClone = cargoStack.clone();
+                                            itemClone.setAmount(limit);
+                                            inv.addItem(itemClone);
+                                            p.getInventory().removeItem(itemClone);
+                                        }
+                                    }}
+                            }
+                        }
+                    }
+                    return;
+
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlaceEvent(BlockPlaceEvent e){
+        if(e.getBlock().getType().equals(Material.SIGN) || e.getBlock().getType().equals(Material.WALL_SIGN) || e.getBlock().getType().equals(Material.SIGN_POST)){
+            Sign sign = (Sign)(e.getBlock().getState());
+            if(ChatColor.stripColor(sign.getLine(0)).equalsIgnoreCase("[Load]") || ChatColor.stripColor(sign.getLine(0)).equalsIgnoreCase("[UnLoad]")){
+                if((!sign.getLine(2).trim().equals("") && Utils.isInventoryHolder(Material.getMaterial(sign.getLine(1)))) && (!sign.getLine(2).trim().equals("") && Utils.isInventoryHolder(Material.getMaterial(sign.getLine(2))))){
+                    sign.setLine(0,ChatColor.GREEN + ChatColor.stripColor(sign.getLine(0)));
+                }else{
+                    sign.setLine(0,ChatColor.RED + ChatColor.stripColor(sign.getLine(0)));
+                }
+            }
+        }
+    }
+}
---
+++ src/main/io/github/cccm5/Utils.java
@@ -0,0 +1,140 @@
+package io.github.cccm5;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.utils.MovecraftLocation;
+
+public class Utils
+{
+    private static final Material[] INVENTORY_MATERIALS = new Material[]{Material.CHEST,Material.TRAPPED_CHEST, Material.FURNACE, Material.HOPPER,Material.DROPPER,Material.DISPENSER, Material.BREWING_STAND};
+    /**
+     * Check if a material's block form can contain an inventory
+     * 
+     * @param material the material to check
+     * @return true if the material can contain an inventory, otherwise false
+     */
+    public static boolean isInventoryHolder(Material material){
+        for(Material mat : INVENTORY_MATERIALS){
+            if(material.equals(mat))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if a block can contain an inventory
+     * 
+     * @param block the block to check
+     * @return true if the block can contain an inventory, otherwise false
+     */
+    public static boolean isInventoryHolder(Block block){
+        return isInventoryHolder(block.getType());
+    }
+
+    public static ArrayList<Inventory> getInventorysOnCraft(Craft craft, ItemStack i){
+        return getInventorysOnCraft(craft,i,null);
+    }
+
+    public static ArrayList<Inventory> getInventorysOnCraft(Craft craft, ItemStack i,List<Material> lookup){
+        ArrayList<Inventory> inventories= new ArrayList<Inventory>();
+        for(Location loc : movecraftLocationToBukkitLocation(craft.getBlockList(),craft.getW()))
+            if(isInventoryHolder(loc.getBlock()))
+                if(lookup==null || lookup.size()==0)
+                    inventories.add( ((InventoryHolder)loc.getBlock().getState()).getInventory());
+                else
+                    for(Material mat : lookup)
+                        if(loc.getBlock().getType()==mat)
+                            inventories.add( ((InventoryHolder)loc.getBlock().getState()).getInventory());
+        return inventories;
+    }
+
+    /**
+     * Converts a movecraftLocation Object to a bukkit Location Object
+     * 
+     * @param movecraftLoc the movecraft location to be converted
+     * @param world the world of the location
+     * @return the converted location
+     */
+    public static Location movecraftLocationToBukkitLocation(MovecraftLocation movecraftLoc, World world){
+        return new Location(world,movecraftLoc.getX(),movecraftLoc.getY(),movecraftLoc.getZ());
+    }
+
+    /**
+     * Converts a list of movecraftLocation Object to a bukkit Location Object
+     * 
+     * @param movecraftLocations the movecraftLocations to be converted
+     * @param world the world of the location
+     * @return the converted location
+     */
+    public static ArrayList<Location> movecraftLocationToBukkitLocation(List<MovecraftLocation> movecraftLocations, World world){
+        ArrayList<Location> locations = new ArrayList<Location>();
+        for(MovecraftLocation movecraftLoc : movecraftLocations){
+            locations.add(movecraftLocationToBukkitLocation(movecraftLoc,world));
+        }
+        return locations;
+    }
+
+    /**
+     * Converts a list of movecraftLocation Object to a bukkit Location Object
+     * 
+     * @param movecraftLocations the movecraftLocations to be converted
+     * @param world the world of the location
+     * @return the converted location
+     */
+    public static ArrayList<Location> movecraftLocationToBukkitLocation(MovecraftLocation[] movecraftLocations, World world){
+        ArrayList<Location> locations = new ArrayList<Location>();
+        for(MovecraftLocation movecraftLoc : movecraftLocations){
+            locations.add(movecraftLocationToBukkitLocation(movecraftLoc,world));
+        }
+        return locations;
+    }
+
+    public static int getItemCount(Inventory inv, ItemStack item){
+        int count=0;
+        for(ItemStack tempStack : inv.getContents())
+            if(tempStack.isSimilar(item))
+                count+=tempStack.getAmount();
+        return count;   
+    }
+
+    public static boolean hasSpace(Inventory inv, ItemStack item){
+        if(inv.firstEmpty()!=-1)
+            return true;
+
+        int occurance = 0;
+        int count = 0;
+        for(ItemStack tempStack : inv.getContents())
+            if(tempStack.isSimilar(item)){
+                occurance++;
+                count+=tempStack.getAmount();
+            }
+
+        if(((double)(count+item.getAmount()))/occurance < item.getMaxStackSize())
+            return true;
+        return false;
+    }
+
+    public static int addLimit(Inventory inv, ItemStack item){      
+        int count = 0;
+        for(ItemStack tempStack : inv.getContents())
+        {
+            if(tempStack == null)
+                count+=item.getMaxStackSize();
+            if(tempStack.isSimilar(item)){
+                count+=tempStack.getMaxStackSize()-tempStack.getAmount();
+            }
+        }
+        return count;
+    }
+}
