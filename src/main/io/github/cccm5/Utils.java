ac09719a7f3bf754d1cb24c7840dc94b406ec958
cccm5 <ccccm5@gmail.com>
2016-11-13 17:17:33 -0500

    Remove unneeded utility methods

--- src/main/io/github/cccm5/Utils.java
+++ src/main/io/github/cccm5/Utils.java
@@ -20,54 +20,6 @@ import net.citizensnpcs.api.trait.Trait;
 public class Utils
 {
     private static final Material[] INVENTORY_MATERIALS = new Material[]{Material.CHEST,Material.TRAPPED_CHEST, Material.FURNACE, Material.HOPPER,Material.DROPPER,Material.DISPENSER, Material.BREWING_STAND};
-    /**
-     * Check if a material's block form can contain an inventory
-     * 
-     * @param material the material to check
-     * @return true if the material can contain an inventory, otherwise false
-     */
-    public static boolean isInventoryHolder(Material material){
-        for(Material mat : INVENTORY_MATERIALS){
-            if(material.equals(mat))
-                return true;
-        }
-        return false;
-    }
-
-    public static boolean isInventoryHolder(String type){
-        if(Material.getMaterial(type.toUpperCase().replaceAll(" ","_"))!=null)
-            return isInventoryHolder(Material.getMaterial(type.toUpperCase().replaceAll(" ","_")));
-        return false;
-    }
-
-    /**
-     * Check if a block can contain an inventory
-     * 
-     * @param block the block to check
-     * @return true if the block can contain an inventory, otherwise false
-     */
-    public static boolean isInventoryHolder(Block block){
-        if(block==null)
-            return false;
-        return isInventoryHolder(block.getType());
-    }
-
-    public static ArrayList<Inventory> getInventorysOnCraft(Craft craft, ItemStack i){
-        return getInventorysOnCraft(craft,i,null);
-    }
-
-    public static ArrayList<Inventory> getInventorysOnCraft(Craft craft, ItemStack i,List<Material> lookup){
-        ArrayList<Inventory> inventories= new ArrayList<Inventory>();
-        for(Location loc : movecraftLocationToBukkitLocation(craft.getBlockList(),craft.getW()))
-            if(isInventoryHolder(loc.getBlock()))
-                if(lookup==null || lookup.size()==0)
-                    inventories.add( ((InventoryHolder)loc.getBlock().getState()).getInventory());
-                else
-                    for(Material mat : lookup)
-                        if(loc.getBlock().getType()==mat)
-                            inventories.add( ((InventoryHolder)loc.getBlock().getState()).getInventory());
-        return inventories;
-    }
 
     /**
      * Converts a movecraftLocation Object to a bukkit Location Object
@@ -110,46 +62,6 @@ public class Utils
         return locations;
     }
 
-    public static int getItemCount(Inventory inv, ItemStack item){
-        int count=0;
-        for(ItemStack tempStack : inv.getContents())
-            if(tempStack.isSimilar(item))
-                count+=tempStack.getAmount();
-        return count;   
-    }
-
-    public static boolean hasSpace(Inventory inv, ItemStack item){
-        if(inv.firstEmpty()!=-1)
-            return true;
-
-        int occurance = 0;
-        int count = 0;
-        for(ItemStack tempStack : inv.getContents())
-            if(tempStack.isSimilar(item)){
-                occurance++;
-                count+=tempStack.getAmount();
-            }
-
-        if(((double)(count+item.getAmount()))/occurance < item.getMaxStackSize())
-            return true;
-        return false;
-    }
-
-    public static int addLimit(Inventory inv, ItemStack item){
-        if(item == null)
-            return 0;
-        int count = 0;
-        for(ItemStack tempStack : inv.getContents())
-        {
-            if(tempStack == null)
-                count+=item.getMaxStackSize();
-            else if(tempStack.isSimilar(item)){
-                count+=tempStack.getMaxStackSize()-tempStack.getAmount();
-            }
-        }
-        return count;
-    }
-
     public static ArrayList<NPC> getNPCsWithTrait(Class<? extends Trait> c){
         ArrayList<NPC> npcs = new ArrayList<NPC>();
         for(NPCRegistry registry : net.citizensnpcs.api.CitizensAPI.getNPCRegistries())
@@ -182,7 +94,7 @@ public class Utils
         }
         if(craft == null)
             throw new IllegalArgumentException("craft must not be null");
-            
+
         for(Location loc : movecraftLocationToBukkitLocation(craft.getBlockList(),craft.getW()))
             for(Material m : lookup)
                 if(loc.getBlock().getType() == m)
@@ -221,7 +133,7 @@ public class Utils
             throw new IllegalArgumentException("craft must not be null");
         if(item.getType() == Material.AIR)
             throw new IllegalArgumentException("item must not have type Material.AIR");
-            
+
         for(Location loc : movecraftLocationToBukkitLocation(craft.getBlockList(),craft.getW()))
             for(Material m : lookup)
                 if(loc.getBlock().getType() == m)
@@ -235,5 +147,5 @@ public class Utils
                 }
         return null;
     }
-    
+
 }
